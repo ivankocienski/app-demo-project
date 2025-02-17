6a14c66471b4374a5650505f7015fe1353da8639
Ivan Kocienski <ivan.kocienski@gmail.com>
2025-02-17 12:18:57 +0000

    HTTP JSON server with status endpoint

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "encoding/json"
+
+  "github.com/gorilla/mux"
+)
+
+type StatusPayloadType struct {
+  Status string
+  PartnerCount int
+}
+
+func handleStatus(w http.ResponseWriter, _ *http.Request) {
+  payload := StatusPayloadType{
+    Status: "online",
+    PartnerCount: 1234,
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(payload)
+}
 
 func main() {
-  fmt.Println("Yo")
+  r := mux.NewRouter()
+
+  r.HandleFunc("/api/v1/status", handleStatus).Methods("GET")
+
+  fmt.Println("Starting on http://localhost:8002")
+  http.ListenAndServe(":8002", r)
 }
